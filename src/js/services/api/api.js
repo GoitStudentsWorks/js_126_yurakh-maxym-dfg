9c0e46b2d06321435c275c2d7c08f45abe212edb
Daniel <djdanilko02@gmail.com>
2026-05-21 18:20:58 +0200

    add-baseURL

---
+++ src/js/services/api/api.js
@@ -0,0 +1,17 @@
+const BASE_URL =
+  'https://deserts-store.b.goit.study/api';
+
+export async function request(endpoint, options = {}) {
+  const response = await fetch(
+    `${BASE_URL}${endpoint}`,
+    options
+  );
+
+  if (!response.ok) {
+    throw new Error(
+      `HTTP Error: ${response.status}`
+    );
+  }
+
+  return response.json();
+}
