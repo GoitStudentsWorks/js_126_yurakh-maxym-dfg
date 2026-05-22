8e5f6029be10ba69b3a655ea69b60cdfb5fd72db
Oleksandr <zombozo223@gmail.com>
2026-05-22 21:01:33 +0300

    add about-us

---
+++ src/js/features/about-us.js
@@ -0,0 +1,36 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.mySwiper', {
+  modules: [Navigation, Pagination], 
+
+  loop: true, 
+
+  slidesPerView: 1,
+  spaceBetween: 16,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true, 
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1440: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    }
+  }
+});
