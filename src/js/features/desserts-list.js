5b2726c5d057f9e26754aed07dd7b85e0e2f586d
Daniel <djdanilko02@gmail.com>
2026-05-23 17:59:22 +0200

    render_Category

--- src/js/features/desserts-list.js
+++ src/js/features/desserts-list.js
@@ -3,14 +3,49 @@ import 'choices.js/public/assets/styles/choices.min.css';
 
 import Raty from 'raty-js';
 
-import {renderCategories} from '/js/render/renderDesserts.js';
+import {
+  renderCategories,
+  renderDesserts,
+} from '../render/renderDesserts.js';
 import {getCategories} from '/js/services/api/categories.js';
+import { getDesserts } from '../services/api/desserts.js';
 
 async function initDesserts() {
   const categories = await getCategories();
 
   renderCategories(categories);
+
+ const dessertsData = await getDesserts();
+
+renderDesserts(dessertsData.desserts);
+
+   const container = document.querySelector(
+    '.desserts_categories-container'
+  );
+
+  container.addEventListener('change', handleCategoryChange);
+
+
 }
 
+async function handleCategoryChange(event) {
+  const categoryId = event.target.value;
+
+  let data;
+
+  if (categoryId === 'all') {
+    data = await getDesserts();
+  } else {
+    data = await getDesserts({
+      category: categoryId,
+    });
+  }
+
+  renderDesserts(data.desserts);
+}
+
+
 initDesserts();
+
+
 
--- src/js/render/renderDesserts.js
+++ src/js/render/renderDesserts.js
@@ -3,18 +3,62 @@ export function renderCategories(categories) {
     '.desserts_categories-container'
   );
 
-  const markup = categories
+const categoriesWithAll = [
+  {
+    _id: 'all',
+    name: 'Всі десерти',
+  },
+  ...categories,
+];
+
+  const markup = categoriesWithAll
     .map(
-      ({ _id, name }) => `
-    <label>
-      <input
-        type="radio"
-        name="category"
-        value="${_id}"
-      >
-      ${name}
-    </label>
-  `
+      ({ _id, name }, index) => `
+      <label>
+        <input
+          type="radio"
+          name="category"
+          value="${_id}"
+          ${index === 0 ? 'checked' : ''}
+        >
+        ${name}
+      </label>
+    `
+    )
+    .join('');
+
+  container.innerHTML = markup;
+}
+
+export function renderDesserts(desserts) {
+  const container = document.querySelector(
+    '.desserts-list'
+  );
+
+  const markup = desserts
+    .map(
+      ({
+        image,
+        category,
+        name,
+        description,
+        price,
+      }) => `
+        <li class="dessert-card">
+          <img
+            src="${image}"
+            alt="${name}"
+          >
+
+          <p>${category.name}</p>
+
+          <h3>${name}</h3>
+
+          <p>${description}</p>
+
+          <p>${price} грн</p>
+        </li>
+      `
     )
     .join('');
 
