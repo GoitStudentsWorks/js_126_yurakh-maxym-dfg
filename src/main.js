b0f78309a5a36b1b42de982ea4bf1f038c867b3b
Daryna <darina.savarina31@gmail.com>
2026-05-25 22:49:55 +0200

    add footer.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './js/features/desserts-list.js';
 import './js/features/about-us.js';
 import './js/features/faq.js';
 import './js/features/feedbacks.js';
+import './js/features/footer.js';
 
 import './js/modal/mobile-menu.js';
 import './js/modal/dessert-details-modal.js';
