6f5487968a403d50adb117da94e66be4edd084db
Daryna <darina.savarina31@gmail.com>
2026-05-25 19:04:13 +0200

    add modal

--- src/js/modal/order-modal.js
+++ src/js/modal/order-modal.js
@@ -1,18 +1,58 @@
-(() => {
-  const refs = {
-      // Додати атрибут data-order-open на кнопку відкриття
-      openModalBtn: document.querySelector('[data-order-open]'),
-      // Додати атрибут data-order-close на кнопку закриття
-      closeModalBtn: document.querySelector('[data-order-close]'),
-      // Додати атрибут data-order на div-контейнер модалки
-      modal: document.querySelector('[data-order]'),
+import { createOrder } from '../services/api/orders.js';
+
+const refs = {
+  closeModalBtn: document.querySelector('[data-order-close]'),
+  modal: document.querySelector('[data-order]'),
+  form: document.querySelector('.order-form'),
+};
+
+window.addEventListener('order:open', event => {
+  const { dessertId } = event.detail;
+
+  refs.modal.classList.remove('is-hidden');
+  refs.modal.dataset.dessertId = dessertId;
+
+  document.addEventListener('keydown', onEscPress);
+});
+
+refs.closeModalBtn.addEventListener('click', closeModal);
+
+refs.modal.addEventListener('click', event => {
+  if (event.target === refs.modal) {
+    closeModal();
+  }
+});
+
+function closeModal() {
+  refs.modal.classList.add('is-hidden');
+  document.removeEventListener('keydown', onEscPress);
+}
+
+function onEscPress(event) {
+  if (event.code === 'Escape') {
+    closeModal();
+  }
+}
+
+refs.form.addEventListener('submit', async event => {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+
+  const orderData = {
+    dessertId: refs.modal.dataset.dessertId,
+    name: formData.get('name'),
+    phone: formData.get('phone'),
+    comment: formData.get('comment'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    // is-order-open це клас який буде додаватися/забиратися на div-контейнері при натисканні на кнопки
-    refs.modal.classList.toggle('is-order-open');
+  try {
+    await createOrder(orderData);
+    alert('Замовлення успішно оформлено!');
+    refs.form.reset();
+    closeModal();
+  } catch (error) {
+    console.error(error);
+    alert('Щось пішло не так при відправці замовлення.');
   }
-})();
+});
